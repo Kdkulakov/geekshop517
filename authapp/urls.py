f962807f859b09b06a24f924a335583fc2375acd
Kulakov <kdkulakov@gmail.com>
2021-03-27 20:14:22 +0300

    init

---
+++ authapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from authapp.views import GeekLoginView, RegisterView, ProfileView, logout
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', GeekLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('logout/', logout, name='logout'),
+    path('verify/<str:email>/<str:activation_key>/', RegisterView.verify, name='verify'),
+]
