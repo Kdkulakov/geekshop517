c4d150b4965f5c9c3bdb380c461c71abc65210e4
Kulakov <kdkulakov@gmail.com>
2021-04-11 15:55:28 +0300

    add static root

--- mainapp/views.py
+++ mainapp/views.py
@@ -15,6 +15,7 @@ def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
     if category_id:
         products = Product.objects.filter(category_id=category_id).order_by('-price')
+        products2 = Product.objects.filter(category_id=category_id).order_by('-price')
     else:
         products = Product.objects.all().order_by('-price')
     paginator = Paginator(products, 3)
