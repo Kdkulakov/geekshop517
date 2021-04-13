5099697df94b7044163376eccae6fe33ac54406e
Kulakov <kdkulakov@gmail.com>
2021-04-13 21:33:23 +0300

    django extensions

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from mainapp.views import products, ProductList, ProductAdminList, main
+from mainapp.views import products, ProductList, ProductAdminList
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', main, name='index'),
+    path('', ProductList.as_view(), name='index'),
     path('admin_products/', ProductAdminList.as_view(), name='admin_products'),
     path('<int:category_id>/', ProductList.as_view(), name='product'),
     path('page/<int:page>/', products, name='page')
