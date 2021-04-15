16b81746ab2a0b72af89559c314e56d5ea584323
Kulakov <kdkulakov@gmail.com>
2021-04-15 22:20:41 +0300

    add memc

--- mainapp/views.py
+++ mainapp/views.py
@@ -131,37 +131,37 @@ class ProductAdminList(LoginRequiredMixin, ListView):
 
 
 def products_ajax(request, pk=None, page=1):
-    if request.is_ajax():
-        links_menu = get_links_menu()
 
-        if pk:
-            if pk == '0':
-                category = {
-                   'pk': 0,
-                   'name': 'все'
-                }
-                products = get_products_ordered_by_price()
-            else:
-                category = get_category(pk)
-                products = get_products_in_category_ordered_by_price(pk)
+    links_menu = get_links_menu()
 
-            paginator = Paginator(products, 2)
-            try:
-                products_paginator = paginator.page(page)
-            except PageNotAnInteger:
-                products_paginator = paginator.page(1)
-            except EmptyPage:
-                products_paginator = paginator.page(paginator.num_pages)
-
-            content = {
-               'links_menu': links_menu,
-               'category': category,
-               'products': products_paginator,
+    if pk:
+        if pk == '0':
+            category = {
+               'pk': 0,
+               'name': 'все'
             }
+            products = get_products_ordered_by_price()
+        else:
+            category = get_category(pk)
+            products = get_products_in_category_ordered_by_price(pk)
 
-            result = render_to_string(
-                        'mainapp/includes/inc_products_list_content.html',
-                        context=content,
-                        request=request)
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
 
-            return JsonResponse({'result': result})
+        content = {
+           'links_menu': links_menu,
+           'category': category,
+           'products': products_paginator,
+        }
+
+        result = render_to_string(
+                    'mainapp/includes/inc_products_list_content.html',
+                    context=content,
+                    request=request)
+
+        return JsonResponse({'result': result})
