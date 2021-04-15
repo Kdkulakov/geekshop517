96890f8a32c91404d4552cf85c1ef1cd892dea29
Kulakov <kdkulakov@gmail.com>
2021-04-15 21:59:07 +0300

    add memc

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from mainapp.views import products, ProductList, ProductAdminList
+from mainapp.views import products, ProductList, ProductAdminList, get_products
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', ProductList.as_view(), name='index'),
+    # path('', ProductList.as_view(), name='index'),
+    path('', get_products, name='index'),
     path('admin_products/', ProductAdminList.as_view(), name='admin_products'),
     path('<int:category_id>/', ProductList.as_view(), name='product'),
     path('page/<int:page>/', products, name='page')
